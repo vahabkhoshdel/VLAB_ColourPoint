5d256f8b50ebbbf3097af07c2cff279ad8993965
vahabkhoshdel <70492659+vahabkhoshdel@users.noreply.github.com>
2025-07-28 07:49:16 -0700

    Add files via upload

---
+++ test_gen.sh
@@ -0,0 +1,6 @@
+python test_gen.py \
+--ckpt ./logs_gen/GEN_2025_07_13__21_22_49/ckpt_0.000000_41000.pt \
+--dataset_path "data/plant3d.hdf5" \
+--batch_size 16 \
+--num_classes 16 \
+--class_label 0 \
---
+++ train_gen.sh
@@ -0,0 +1,5 @@
+python train_gen.py \
+--train_batch_size 16 \
+--val_batch_size 16 \
+--num_classes 16 \
+--dataset_path "data/densenet.hdf5" 
